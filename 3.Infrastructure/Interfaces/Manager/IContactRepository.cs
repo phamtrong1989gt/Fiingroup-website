e89a4fd035573ee7151655be055d8faaf0365a4d
bkitforyou <bkitforyou@outlook.com>
2025-11-20 16:50:48 +0700

    add

--- 3.Infrastructure/Interfaces/Manager/IContactRepository.cs
+++ 3.Infrastructure/Interfaces/Manager/IContactRepository.cs
@@ -9,5 +9,6 @@ namespace PT.Infrastructure.Interfaces
     {
         Task<SelectList> ServiesList(string language, int portalId, int? parrentId = null);
         Task<List<ContentPage>> FlowSelectList(string language, int portalId, int parrentId);
+        Task<SelectList> ServiesNewsList(string language, int portalId);
     }
 }
