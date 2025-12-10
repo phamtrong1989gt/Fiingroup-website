68f6e5d5773515e411051fb977bfd89b565c58d5
TRONG <TRONG@DESKTOP-MFV7957>
2025-12-10 22:42:17 +0700

    u

--- 6.FE/Portal2/Models/ContactHomeModel.cs
+++ 6.FE/Portal2/Models/ContactHomeModel.cs
@@ -9,74 +9,83 @@ namespace PT.UI.Models
 {
     public class ContactHomeModel
     {
+        [Display(Name = nameof(Id))]
         public int Id { get; set; }
 
-        [Display(Name = "Họ và tên")]
+        [Display(Name = nameof(FullName))]
         [Required(ErrorMessage = "{0}  không được để trống")]
         [StringLength(50, MinimumLength = 10, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string FullName { get; set; }
 
-        [Display(Name = "Số điện thoại")]
+        [Display(Name = nameof(Phone))]
         [Required(ErrorMessage = "{0}  không được để trống")]
         [StringLength(20, MinimumLength = 8, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string Phone { get; set; }
 
-        [Display(Name = "Email")]
+        [Display(Name = nameof(Email))]
         [Required(ErrorMessage = "{0}  không được để trống")]
         [StringLength(100, MinimumLength = 10, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string Email { get; set; }
 
-        [Display(Name = "Tên công ty")]
+        [Display(Name = nameof(ConpanyName))]
         [Required(ErrorMessage = "{0}  không được để trống")]
         [StringLength(100, MinimumLength = 1, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string ConpanyName { get; set; }
 
-        [Display(Name = "Chức danh công việc")]
+        [Display(Name = nameof(Position))]
         [Required(ErrorMessage = "{0}  không được để trống")]
         [StringLength(50, MinimumLength = 1, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string Position { get; set; }
 
-        [Display(Name = "Mô tả chi tiết")]
+        [Display(Name = nameof(Content))]
         [StringLength(500, MinimumLength = 0, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string Content { get; set; }
 
+        [Display(Name = nameof(Language))]
         public string Language { get; set; }
 
+        [Display(Name = nameof(Capcha))]
         public string Capcha { get; set; }
 
-        [Display(Name = "Nhóm ngành")]
+        [Display(Name = nameof(ServiceId))]
         [Required(ErrorMessage = "{0} không được để trống")]
         public int? ServiceId { get; set; }
 
-        [Display(Name = "Sản phẩm & dịch vụ quan tâm")]
+        [Display(Name = nameof(Products))]
         [StringLength(100, MinimumLength = 0, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string Products { get; set; }
 
-        public SelectList CountrySelectlist { get;  set; }
+        [Display(Name = nameof(CountrySelectlist))]
+        public SelectList CountrySelectlist { get; set; }
     }
 
     public class ContactSolotionModel
     {
+        [Display(Name = nameof(Id))]
         public int Id { get; set; }
-        [Display(Name = "Họ và tên")]
+
+        [Display(Name = nameof(FullName))]
         [Required(ErrorMessage = "{0}  không được để trống")]
         [StringLength(50, MinimumLength = 10, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string FullName { get; set; }
-        [Display(Name = "Số điện thoại")]
+
+        [Display(Name = nameof(Phone))]
         [Required(ErrorMessage = "{0}  không được để trống")]
         [StringLength(20, MinimumLength = 10, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string Phone { get; set; }
 
-        [Display(Name = "Email")]
+        [Display(Name = nameof(Email))]
         [Required(ErrorMessage = "{0}  không được để trống")]
         [StringLength(100, MinimumLength = 10, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string Email { get; set; }
 
+        [Display(Name = nameof(Language))]
         public string Language { get; set; }
 
+        [Display(Name = nameof(Capcha))]
         public string Capcha { get; set; }
 
-        [Display(Name = "Nhận tin tức liên quan đến lĩnh vực")]
+        [Display(Name = nameof(Products))]
         [StringLength(100, MinimumLength = 0, ErrorMessage = "{0} phải có độ dài từ {2} đến {1} ký tự.")]
         public string Products { get; set; }
     }
