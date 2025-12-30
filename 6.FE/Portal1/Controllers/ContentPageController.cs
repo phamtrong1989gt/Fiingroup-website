73d8c60ebca461bfc2064803b3d74792d31d88e8
phamtrong1989gt <43569848+phamtrong1989gt@users.noreply.github.com>
2025-12-30 20:59:54 +0700

    u

--- 6.FE/Portal1/Controllers/ContentPageController.cs
+++ 6.FE/Portal1/Controllers/ContentPageController.cs
@@ -82,7 +82,7 @@ namespace PT.UI.Controllers
             };
             ViewData["language"] = prs.Language;
             var param = await _iSettingService.ParameterGet(parameter, _baseSettings.Value.PortalId, language);
-            if(param == null)
+            if(param == null || param.Value == null)
             {
                 return View(view, null);
             }    
